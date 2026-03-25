df7f2f03e0218d73e6fe5df42abf43fd4fbbb9f2
Leonardo Paz <leonardofranciscopaz@gmail.com>
2026-03-24 23:13:38 -0300

    Cambios en los .css de index, app. Cambios en App.jsx y creación del Header

---
+++ my-app/src/pages/header/Header.jsx
@@ -0,0 +1,13 @@
+import './header.css'
+
+function Header() {
+
+    return(
+        <div className='container'>
+            <h1>Mi Header</h1>
+            <p>Texto de ejemplo encima de la imagen de fondo.</p>
+        </div>
+    );
+}
+
+export default Header;
