e139bdf0f05a8140ade154821125200613cb663c
Franco Villegas <afdvillegas@gmail.com>
2026-03-24 23:46:49 -0300

    Actualizao App.jsx con componente Catalogo

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,13 +2,13 @@ import './App.css'
 import { Slider } from '@chakra-ui/react'
 import Footer from './pages/footer/Footer'
 import Header from './pages/header/Header'
-
+import Catalogo from './pages/catalogo/Catalogo'
 function App() {
 
   return (
     <>
       <Header/>
-      
+      <Catalogo/>
       <Footer />
     </>
   )
